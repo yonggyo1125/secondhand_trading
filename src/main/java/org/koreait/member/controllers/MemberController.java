8d1cee64c3b2b651e72f0ea50f6581342086646f
yonggyo1125 <yonggyo00@kakao.com>
2025-05-26 17:04:11 +0900

    .

--- src/main/java/org/koreait/member/controllers/MemberController.java
+++ src/main/java/org/koreait/member/controllers/MemberController.java
@@ -1,15 +1,22 @@
 package org.koreait.member.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/member")
 public class MemberController {
 
     @GetMapping("/join")
-    public String join() {
+    public String join(Model model) {
+
+        model.addAttribute("addCss", List.of("member/style", "member/test"));
+        model.addAttribute("addScript", List.of("member/form", "member/test2"));
+
         return "front/member/join";
     }
 }
