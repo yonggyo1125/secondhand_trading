c99d0e7cbceeb2299e20378be8626f75fed79cc6
yonggyo1125 <yonggyo00@kakao.com>
2025-05-27 17:12:28 +0900

    .

--- src/main/java/org/koreait/member/controllers/MemberController.java
+++ src/main/java/org/koreait/member/controllers/MemberController.java
@@ -6,12 +6,14 @@ import org.koreait.global.libs.Utils;
 import org.koreait.member.validators.JoinValidator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -30,17 +32,15 @@ public class MemberController {
     // 회원가입 양식
     @GetMapping("/join")
     public String join(@ModelAttribute RequestJoin form, Model model) {
-
-        model.addAttribute("addCommonScript", List.of("fileManager"));
-        model.addAttribute("addScript", List.of("member/join"));
-        model.addAttribute("pageTitle", "회원가입");
+        commonProcess("join", model);
 
         return utils.tpl("member/join");
     }
 
     // 회원가입 처리
     @PostMapping("/join")
-    public String joinPs(@Valid RequestJoin form, Errors errors) {
+    public String joinPs(@Valid RequestJoin form, Errors errors, Model model) {
+        commonProcess("join", model);
 
         joinValidator.validate(form, errors);
 
@@ -53,8 +53,35 @@ public class MemberController {
     }
 
     @GetMapping("/login")
-    public String login(@ModelAttribute RequestLogin form) {
+    public String login(@ModelAttribute RequestLogin form, Model model) {
+        commonProcess("login", model);
 
         return utils.tpl("member/login");
+    }
+
+    /**
+     * 현재 컨트롤러의 공통 처리 부분
+     *
+     * @param mode
+     * @param model
+     */
+    private void commonProcess(String mode, Model model) {
+        mode = StringUtils.hasText(mode) ? mode : "join";
+        String pageTitle = "";
+        List<String> addCommonScript = new ArrayList<>();
+        List<String> addScript = new ArrayList<>();
+
+        if (mode.equals("join")) { // 회원 가입 공통 처리
+            addCommonScript.add("fileManager");
+            addScript.add("member/join");
+            pageTitle = "회원가입";
+
+        } else if (mode.equals("login")) { // 로그인 공통 처리
+            pageTitle = "로그인";
+        }
+
+        model.addAttribute("addCommonScript", addCommonScript);
+        model.addAttribute("addScript", addScript);
+        model.addAttribute("pageTitle", pageTitle);
     }
 }
