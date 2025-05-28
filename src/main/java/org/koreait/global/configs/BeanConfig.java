920eba1a3616cebe56c9e8e9c6b98a220ac20236
yonggyo1125 <yonggyo00@kakao.com>
2025-05-28 15:57:00 +0900

    .

---
+++ src/main/java/org/koreait/global/configs/BeanConfig.java
@@ -0,0 +1,19 @@
+package org.koreait.global.configs;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+@Configuration
+public class BeanConfig {
+    @Lazy
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        return mapper;
+    }
+}
--- src/main/java/org/koreait/member/services/JoinService.java
+++ src/main/java/org/koreait/member/services/JoinService.java
@@ -1,14 +1,37 @@
 package org.koreait.member.services;
 
+import lombok.RequiredArgsConstructor;
 import org.koreait.member.controllers.RequestJoin;
+import org.koreait.member.entities.Member;
+import org.koreait.member.repositories.MemberRepository;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 @Lazy
 @Service
+@RequiredArgsConstructor
 public class JoinService {
 
-    public void process(RequestJoin form) {
+    private final PasswordEncoder encoder;
+    private final MemberRepository repository;
 
+    public void process(RequestJoin form) {
+        /**
+         * 1. 비밀번호를 BCrypt 해시화
+         * 2. 휴대전화번호 통일화, 010-1000-1000, 01010001000, 010.1000.1000
+         *      - 숫자만 남기고 다 제거
+         * 3. DB에 영구 저장
+         */
+
+        String hash = encoder.encode(form.getPassword());
+        String mobile = form.getMobile();
+        if (StringUtils.hasText(mobile)) {
+            mobile = mobile.replaceAll("\\D", "");
+        }
+
+        Member member = new Member();
+        member.setEmail(form.getEmail());
     }
 }
