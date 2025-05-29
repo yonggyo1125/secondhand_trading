f71fc4c1fccbd750f37d5ead849cd895e4603450
yonggyo1125 <yonggyo00@kakao.com>
2025-05-29 15:42:39 +0900

    .

--- src/main/resources/static/common/js/common.js
+++ src/main/resources/static/common/js/common.js
@@ -1,7 +1,11 @@
 /* window.alert를 SweetAlert2로 교체 */
-window.alert = function(message) {
-    Swal.fire({
+window.alert = function(message, callback) {
+    parent.Swal.fire({
       title: message,
       icon: "warning"
-    });
+    }).then(() -> {
+        if (typeof callback === 'function') {
+            callback();
+        }
+    })
 };
