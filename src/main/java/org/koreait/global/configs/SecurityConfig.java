fbf193da7d5e836127363247f0afc42675528a99
yonggyo1125 <yonggyo00@kakao.com>
2025-05-29 16:22:08 +0900

    .

--- src/main/java/org/koreait/global/configs/SecurityConfig.java
+++ src/main/java/org/koreait/global/configs/SecurityConfig.java
@@ -12,6 +12,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+        /* 인증 설정 - 로그인, 로그아웃 S */
+        http.formLogin(c -> {
+            c.loginPage("/member/login")
+                    .usernameParameter("email")
+                    .passwordParameter("password")
+                    .defaultSuccessUrl("/")
+                    .failureUrl("/member/login");
+        });
+        /* 인증 설정 - 로그인, 로그아웃 E */
+
         return http.build();
     }
 
