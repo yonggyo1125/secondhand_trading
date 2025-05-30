ffb64414f1defbd86a1b4df23cdc247b762d0015
yonggyo1125 <yonggyo00@kakao.com>
2025-05-30 17:09:59 +0900

    .

--- src/main/java/org/koreait/member/libs/MemberUtil.java
+++ src/main/java/org/koreait/member/libs/MemberUtil.java
@@ -1,6 +1,7 @@
 package org.koreait.member.libs;
 
 import org.koreait.member.MemberInfo;
+import org.koreait.member.constants.Authority;
 import org.koreait.member.entities.Member;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.Authentication;
@@ -13,6 +14,11 @@ public class MemberUtil {
     // 로그인 여부
     public boolean isLogin() {
         return getMember() != null;
+    }
+
+    // 관리자 여부
+    public boolean isAdmin() {
+        return isLogin() && getMember().getAuthority() == Authority.ADMIN;
     }
 
     // 로그인한 회원 정보
