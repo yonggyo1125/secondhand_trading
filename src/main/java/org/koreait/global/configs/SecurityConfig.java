89ddf8b46474d7e6c8cf45154eda9a66998d38bf
yonggyo1125 <yonggyo00@kakao.com>
2025-06-02 17:22:31 +0900

    .

--- src/main/java/org/koreait/global/configs/SecurityConfig.java
+++ src/main/java/org/koreait/global/configs/SecurityConfig.java
@@ -1,10 +1,7 @@
 package org.koreait.global.configs;
 
 import lombok.RequiredArgsConstructor;
-import org.koreait.member.services.LoginFailureHandler;
-import org.koreait.member.services.LoginSuccessHandler;
-import org.koreait.member.services.MemberAuthenticationExceptionHandler;
-import org.koreait.member.services.MemberInfoService;
+import org.koreait.member.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -67,7 +64,7 @@ public class SecurityConfig {
 
         http.exceptionHandling(c -> {
            c.authenticationEntryPoint(new MemberAuthenticationExceptionHandler()); // 미로그인 상태에서의 인가 실패에 대한 처리
-            c.accessDeniedHandler() // 인증 받은 회원이 권한이 없는 페이지에 접근한 경우
+            c.accessDeniedHandler(new MemberAccessDeniedHandler()); // 인증 받은 회원이 권한이 없는 페이지에 접근한 경우
         });
         /* 인가 설정 E */
         return http.build();
