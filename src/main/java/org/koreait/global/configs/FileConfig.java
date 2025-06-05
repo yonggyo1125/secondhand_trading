3b9e50b102e70a26e5a4d51d42aa0b3a0932026d
yonggyo1125 <yonggyo00@kakao.com>
2025-06-05 16:38:45 +0900

    .

---
+++ src/main/java/org/koreait/global/configs/FileConfig.java
@@ -0,0 +1,20 @@
+package org.koreait.global.configs;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableConfigurationProperties(FileProperties.class)
+public class FileConfig implements WebMvcConfigurer {
+    private final FileProperties properties;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(properties.getUrl() + "/**")
+                .addResourceLocations("file:///" + properties.getPath() + "/");
+    }
+}
