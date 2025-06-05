0c51c9987d8620f5530a61c52577bf89609eb01a
yonggyo1125 <yonggyo00@kakao.com>
2025-06-05 15:47:41 +0900

    .

--- src/main/java/org/koreait/admin/trend/controllers/TrendController.java
+++ src/main/java/org/koreait/admin/trend/controllers/TrendController.java
@@ -3,6 +3,7 @@ package org.koreait.admin.trend.controllers;
 import lombok.RequiredArgsConstructor;
 import org.koreait.admin.global.controllers.CommonController;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +20,28 @@ public class TrendController extends CommonController {
     }
 
     @GetMapping({"", "/news"}) // /admin/trend, /admin/trend/news
-    public String news() {
+    public String news(Model model) {
+        commonProcess("news", model);
 
         return "admin/trend/news";
+    }
+
+    @GetMapping("/etc")
+    public String etc(Model model) {
+        commonProcess("etc", model);
+
+        return "admin/trend/etc";
+    }
+
+    /**
+     * 공통 처리
+     *
+     * @param code : 서브메뉴 코드
+     * @param model
+     */
+    private void commonProcess(String code, Model model) {
+
+
+        model.addAttribute("subCode", code);
     }
 }
