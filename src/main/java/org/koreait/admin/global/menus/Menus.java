0401016780e03f190160f93a9c7d7bafd6a56e83
yonggyo1125 <yonggyo00@kakao.com>
2025-06-09 14:42:23 +0900

    .

--- src/main/java/org/koreait/admin/global/menus/Menus.java
+++ src/main/java/org/koreait/admin/global/menus/Menus.java
@@ -24,6 +24,12 @@ public class Menus {
            new Menu("news", "뉴스", "/admin/trend"),
            new Menu("etc", "기타", "/admin/trend/etc")
         ));
+
+        // 상품 관리 메뉴
+        menus.put("product", List.of(
+                new Menu("list", "상품목록", "/admin/product"),
+                new Menu("register", "상품등록", "/admin/product/register")
+        ));
     }
 
     /**
