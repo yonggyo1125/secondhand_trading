5e5f523b07f32e968af1976d6b6f8ef27ed771ed
yonggyo1125 <yonggyo00@kakao.com>
2025-06-09 15:01:53 +0900

    .

---
+++ src/main/resources/db/migration/V4__INIT.sql
@@ -0,0 +1,14 @@
+CREATE TABLE PRODUCT (
+    seq BIGINT AUTO_INCREMENT,
+    gid VARCHAR(45) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    category VARCHAR(40),
+    status ENUM('READY', 'SALE', "OUT_OF_STOCK", "STOP"),
+    consumerPrice INT DEFAULT 0,
+    salePrice INT DEFAULT 0,
+    description TEXT,
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    modifiedAt DATETIME,
+    deletedAt DATETIME,
+    PRIMARY KEY(seq)
+);
