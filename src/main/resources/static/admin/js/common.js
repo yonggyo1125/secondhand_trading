3746712e031ba37f209ef50e46230f32de75d4e5
yonggyo1125 <yonggyo00@kakao.com>
2025-06-11 16:57:03 +0900

    .

---
+++ src/main/resources/static/admin/js/common.js
@@ -0,0 +1,27 @@
+window.addEventListener("DOMContentLoaded", function() {
+    // 전체 토글 공통 기능 처리 S
+    const chkAlls = document.getElementsByClassName("check-all");
+    for (const el of chkAlls) {
+        el.addEventListener("click", function() {
+            const { targetName } = this.dataset;
+            const chks = document.getElementsByName(targetName);
+            for (const chk of chks) {
+                chk.checked = this.checked;
+            }
+        });
+    }
+    // 전체 토글 공통 기능 처리 E
+
+    // 공통 양식 처리 S
+    const processFormButtons = document.getElementsByClassName("process-form");
+    for (const el of processFormButtons) {
+        el.addEventListener("click", function() {
+            const method = this.classList.contains("delete") ? "DELETE" : "PATCH";
+            const { formName } = this.dataset;
+            const formEl = document[formName];
+            formEl._method.value = method;
+
+        });
+    }
+    // 공통 양식 처리 E
+});
