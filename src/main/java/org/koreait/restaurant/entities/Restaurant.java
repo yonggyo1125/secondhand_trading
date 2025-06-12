dadf93fba7b5bce3134a60e4b42fc7c52923f49c
yonggyo1125 <yonggyo00@kakao.com>
2025-06-12 11:24:16 +0900

    .

--- src/main/java/org/koreait/restaurant/entities/Restaurant.java
+++ src/main/java/org/koreait/restaurant/entities/Restaurant.java
@@ -2,6 +2,7 @@ package org.koreait.restaurant.entities;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 
 @Data
 public class Restaurant {
@@ -10,6 +11,8 @@ public class Restaurant {
     private String zipcode;
     private String address;
     private String zonecode;
+
+    @Column("roadAddress")
     private String roadAddress;
     private String category;
     private double lat;
