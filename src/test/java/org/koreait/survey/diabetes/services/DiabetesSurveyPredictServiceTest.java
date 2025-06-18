a27a02630d2b7e707ebc7d6745eb427f89c73a6a
yonggyo1125 <yonggyo00@kakao.com>
2025-06-18 11:21:34 +0900

    .

---
+++ src/test/java/org/koreait/survey/diabetes/services/DiabetesSurveyPredictServiceTest.java
@@ -0,0 +1,13 @@
+package org.koreait.survey.diabetes.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DiabetesSurveyPredictServiceTest {
+
+    @Test
+    void test() {
+
+    }
+}
