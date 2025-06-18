1931cb94c00f3ba663348a13742221194ffd68e6
yonggyo1125 <yonggyo00@kakao.com>
2025-06-18 11:12:48 +0900

    .

--- src/main/java/org/koreait/survey/diabetes/services/DiabetesSurveyPredictService.java
+++ src/main/java/org/koreait/survey/diabetes/services/DiabetesSurveyPredictService.java
@@ -5,11 +5,32 @@ import org.koreait.global.configs.PythonProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Lazy
 @Service
 @RequiredArgsConstructor
 @EnableConfigurationProperties(PythonProperties.class)
 public class DiabetesSurveyPredictService {
+    private final PythonProperties properties;
+    private final WebApplicationContext ctx;
 
+    public List<Integer> process(List<List<Number>> items) {
+
+        boolean isProduction = Arrays.stream(ctx.getEnvironment().getActiveProfiles()).anyMatch(s -> s.equals("prod") || s.equals("mac"));
+
+        String activationCommand = null, pythonPath = null;
+        if (isProduction) { // 리눅스 환경, 서비스 환경
+            activationCommand = String.format("%s/activate", properties.getBase2());
+            pythonPath = properties.getBase2() + "/python";
+        } else { // 윈도우즈 환경
+            activationCommand = String.format("%s/activate.bat", properties.getBase2());
+            pythonPath = properties.getBase2() + "/python.exe";
+        }
+
+        return null;
+    }
 }
