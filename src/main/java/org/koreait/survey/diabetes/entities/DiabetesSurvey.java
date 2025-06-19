8aa0e678398897497f08eb520b7028194af96d74
yonggyo1125 <yonggyo00@kakao.com>
2025-06-19 09:57:13 +0900

    .

---
+++ src/main/java/org/koreait/survey/diabetes/entities/DiabetesSurvey.java
@@ -0,0 +1,39 @@
+package org.koreait.survey.diabetes.entities;
+
+import lombok.Data;
+import org.koreait.global.constants.Gender;
+import org.koreait.global.entities.BaseEntity;
+import org.koreait.member.entities.Member;
+import org.koreait.survey.diabetes.constants.SmokingHistory;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@Table("SURVEY_DIABETES")
+public class DiabetesSurvey extends BaseEntity {
+    @Id
+    private Long seq;
+
+    @Column("memberSeq")
+    private Member member;
+
+    private Gender gender;
+    private boolean hypertension;
+
+    @Column("heartDisease")
+    private boolean heartDisease;
+
+    @Column("smokingHistory")
+    private SmokingHistory smokingHistory;
+
+    private double height;
+    private double weight;
+    private double bmi;
+
+    @Column("hbA1c")
+    private double hbA1c;
+
+    @Column("bloodGlucoseLevel")
+    private double bloodGlucoseLevel;
+}
