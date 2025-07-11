70e1c782dc96a35e73791344f3d4649d69dc2084
yonggyo1125 <yonggyo00@kakao.com>
2025-07-11 12:57:59 +0900

    .

--- src/main/java/org/koreait/test/controllers/Test2Controller.java
+++ src/main/java/org/koreait/test/controllers/Test2Controller.java
@@ -1,19 +1,30 @@
 package org.koreait.test.controllers;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.koreait.global.exceptions.BadRequestException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.koreait.global.libs.Utils;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/test2")
+@RequiredArgsConstructor
 public class Test2Controller {
+    private final Utils utils;
 
     @GetMapping
     public void test1() {
         boolean result = false;
         if (!result) {
             throw new BadRequestException("REST 예외 발생!");
+        }
+    }
+
+    @PostMapping
+    public void test2(@Valid @RequestBody RequestTest form, Errors errors) {
+        if (errors.hasErrors()) {
+            throw new BadRequestException(utils.getErrorMessages(errors));
         }
     }
 }
