93cb5f55a34c16868b7d636ea0768ed88a272e17
yonggyo1125 <yonggyo00@kakao.com>
2025-07-11 12:47:24 +0900

    .

--- src/main/java/org/koreait/global/libs/Utils.java
+++ src/main/java/org/koreait/global/libs/Utils.java
@@ -99,8 +99,17 @@ public class Utils {
         // 필드별 검증 실패 메세지  - rejectValue, 커맨드 객체 검증(필드)
         Map<String, List<String>> messages = errors.getFieldErrors()
                     .stream()
-                .collect(Collectors.toMap(FieldError::getField, f -> getMessages(f.getCodes())));
+                .collect(Collectors.toMap(FieldError::getField, f -> getMessages(f.getCodes()), (v1, v2) -> v2));
         // 글로벌 검증 실패 메세지 - reject
+        List<String> gMessages = errors.getGlobalErrors()
+                .stream()
+                .flatMap(g -> getMessages(g.getCodes()).stream()).toList();
+
+        if (!gMessages.isEmpty()) {
+            messages.put("global", gMessages);
+        }
+
+        return messages;
     }
 
     public String getParam(String name) {
