6fd69631a38c23008e88768fabd3148d1753a78b
yonggyo1125 <yonggyo00@kakao.com>
2025-07-11 11:07:19 +0900

    .

--- src/main/java/org/koreait/global/exceptions/BadRequestException.java
+++ src/main/java/org/koreait/global/exceptions/BadRequestException.java
@@ -2,8 +2,15 @@ package org.koreait.global.exceptions;
 
 import org.springframework.http.HttpStatus;
 
+import java.util.List;
+import java.util.Map;
+
 public class BadRequestException extends CommonException {
     public BadRequestException(String message) {
         super(message, HttpStatus.BAD_REQUEST);
+    }
+
+    public BadRequestException(Map<String, List<String>> errorMessages) {
+        super(errorMessages, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/org/koreait/global/exceptions/NotFoundException.java
+++ src/main/java/org/koreait/global/exceptions/NotFoundException.java
@@ -2,6 +2,9 @@ package org.koreait.global.exceptions;
 
 import org.springframework.http.HttpStatus;
 
+import java.util.List;
+import java.util.Map;
+
 public class NotFoundException extends CommonException {
 
     public NotFoundException() {
@@ -13,5 +16,8 @@ public class NotFoundException extends CommonException {
         super(message, HttpStatus.NOT_FOUND);
     }
 
+    public NotFoundException(Map<String, List<String>> errorMessages) {
+        super(errorMessages, HttpStatus.NOT_FOUND);
+    }
 
 }
