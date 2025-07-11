c0a2460fe1fe1fc8e6577003ddca90e80efc6833
yonggyo1125 <yonggyo00@kakao.com>
2025-07-11 11:08:31 +0900

    .

--- src/main/java/org/koreait/global/exceptions/UnAuthorizedException.java
+++ src/main/java/org/koreait/global/exceptions/UnAuthorizedException.java
@@ -2,6 +2,9 @@ package org.koreait.global.exceptions;
 
 import org.springframework.http.HttpStatus;
 
+import java.util.List;
+import java.util.Map;
+
 public class UnAuthorizedException extends CommonException {
     public UnAuthorizedException() {
         this("UnAuthorized");
@@ -10,5 +13,9 @@ public class UnAuthorizedException extends CommonException {
 
     public UnAuthorizedException(String message) {
         super(message, HttpStatus.UNAUTHORIZED);
+    }
+
+    public UnAuthorizedException(Map<String, List<String>> errorMessages) {
+        super(errorMessages, HttpStatus.UNAUTHORIZED);
     }
 }
