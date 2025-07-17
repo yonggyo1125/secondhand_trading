4bb4f40f369ff27eb04ee2a6b8fb3a2c93542120
yonggyo1125 <yonggyo00@kakao.com>
2025-07-17 13:21:08 +0900

    .

--- src/main/java/org/koreait/member/repositories/MemberRepository.java
+++ src/main/java/org/koreait/member/repositories/MemberRepository.java
@@ -1,11 +1,12 @@
 package org.koreait.member.repositories;
 
 import org.koreait.member.entities.Member;
-import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.Optional;
 
-public interface MemberRepository extends ListCrudRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, QuerydslPredicateExecutor<Member> {
     boolean existsByEmail(String email);
     Optional<Member> findByEmail(String email);
 }
