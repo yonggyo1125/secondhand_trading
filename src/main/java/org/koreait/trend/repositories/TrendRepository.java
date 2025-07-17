af51bfd5b4323ed6dd5caaabb64664bada63b247
yonggyo1125 <yonggyo00@kakao.com>
2025-07-17 15:38:51 +0900

    .

--- src/main/java/org/koreait/trend/repositories/TrendRepository.java
+++ src/main/java/org/koreait/trend/repositories/TrendRepository.java
@@ -1,15 +1,14 @@
 package org.koreait.trend.repositories;
 
+import com.querydsl.core.BooleanBuilder;
 import org.koreait.trend.entities.QTrend;
 import org.koreait.trend.entities.Trend;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -27,12 +26,26 @@ public interface TrendRepository extends JpaRepository<Trend, Long>, QuerydslPre
         return Optional.ofNullable(items.getFirst());
     }
 
-    //@Query("SELECT * FROM TREND WHERE category=:category AND createdAt BETWEEN :sDate AND :eDate ORDER BY createdAt DESC LIMIT 1")
     default Optional<Trend> get(String category, LocalDateTime sDate, LocalDateTime eDate) {
+        QTrend trend = QTrend.trend;
+        BooleanBuilder andBuilder = new BooleanBuilder();
+        andBuilder.and(trend.category.eq(category))
+                .and(trend.createdAt.between(sDate, eDate));
 
-        return null;
+        Pageable pageable = PageRequest.of(0, 1, Sort.by(desc("createdAt")));
+
+        Page<Trend> data = findAll(andBuilder, pageable);
+        List<Trend> items = data.getContent();
+
+        return Optional.ofNullable(items.getFirst());
     }
 
-    @Query("SELECT * FROM TREND WHERE category=:category AND createdAt BETWEEN :sDate AND :eDate")
-    List<Trend> getList(@Param("category") String category, @Param("sDate") LocalDateTime sDate, @Param("eDate") LocalDateTime eDate);
+    default List<Trend> getList(String category, LocalDateTime sDate, LocalDateTime eDate) {
+        QTrend trend = QTrend.trend;
+        BooleanBuilder andBuilder = new BooleanBuilder();
+        andBuilder.and(trend.category.eq(category))
+                .and(trend.createdAt.between(sDate, eDate));
+
+        return (List<Trend>)findAll(andBuilder);
+    }
 }
