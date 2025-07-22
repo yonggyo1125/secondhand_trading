c43a1473fe432af7f056e18e96d3b171dd32eb76
yonggyo1125 <yonggyo00@kakao.com>
2025-07-22 15:20:47 +0900

    .

---
+++ src/main/java/org/koreait/board/repositories/BoardDataRepository.java
@@ -0,0 +1,9 @@
+package org.koreait.board.repositories;
+
+import org.koreait.board.entities.BoardData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+public interface BoardDataRepository extends JpaRepository<BoardData, Long>, QuerydslPredicateExecutor<BoardData> {
+
+}
