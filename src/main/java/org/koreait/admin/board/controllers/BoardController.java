239ce2b3915ef5c1f2e64ee0732697f484a5ca28
yonggyo1125 <yonggyo00@kakao.com>
2025-07-22 15:45:47 +0900

    .

--- src/main/java/org/koreait/admin/board/controllers/BoardController.java
+++ src/main/java/org/koreait/admin/board/controllers/BoardController.java
@@ -3,6 +3,7 @@ package org.koreait.admin.board.controllers;
 import org.koreait.admin.global.controllers.CommonController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,6 +15,30 @@ public class BoardController extends CommonController {
     @ModelAttribute("mainCode")
     public String mainCode() {
         return "board";
+    }
+
+    /**
+     * 게시판 목록
+     * @return
+     */
+    @GetMapping({"", "/list"})
+    public String list(Model model) {
+        commonProcess("list", model);
+
+        return "admin/board/list";
+    }
+
+    /**
+     * 게시판 등록
+     *
+     * @param model
+     * @return
+     */
+    @GetMapping("/register")
+    public String register(Model model) {
+        commonProcess("register", model);
+
+        return "admin/board/register";
     }
 
     /**
