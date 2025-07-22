c764845d1a5d3143d3ddb56126749aca2c9ff356
yonggyo1125 <yonggyo00@kakao.com>
2025-07-22 15:54:19 +0900

    .

--- src/main/java/org/koreait/admin/board/controllers/RequestBoard.java
+++ src/main/java/org/koreait/admin/board/controllers/RequestBoard.java
@@ -1,28 +1,20 @@
-package org.koreait.board.entities;
+package org.koreait.admin.board.controllers;
 
-import jakarta.persistence.*;
 import lombok.Data;
-import org.koreait.global.entities.BaseEntity;
 import org.koreait.member.constants.Authority;
 
 @Data
-@Entity
-public class Board extends BaseEntity {
-    @Id
-    @Column(length=45)
+public class RequestBoard {
     private String bid; // 게시판 아이디
 
-    @Column(length=100, nullable = false)
     private String name; // 게시판 이름
 
     private int rowsForPage; // 한페이지당 레코드 갯수
 
     private int pageCount; // 노출될 페이지 갯수
 
-    @Column(length=45, nullable = false)
     private String skin; // 게시판 스킨
 
-    @Lob
     private String category; // 게시판 분류
 
     private boolean active; // 게시판 사용 여부, true - 사용, false : 미사용
@@ -31,16 +23,11 @@ public class Board extends BaseEntity {
     private boolean attachFile; // 파일 첨부 기능 사용 여부
     private boolean comment; // 댓글 사용 여부
 
-    @Enumerated(EnumType.STRING)
     private Authority listAuthority; // 목록 권한, ALL - 전체, MEMBER - 회원, ADMIN - 관리자
 
-    @Enumerated(EnumType.STRING)
     private Authority viewAuthority; // 글보기 권한
 
-    @Enumerated(EnumType.STRING)
     private Authority writeAuthority; // 글작성 권한
 
-    @Enumerated(EnumType.STRING)
     private Authority commentAuthority; // 댓글 작성 권한
-
 }
