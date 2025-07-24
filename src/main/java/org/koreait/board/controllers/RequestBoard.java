bc0d50347621ceb34bc3d1ef7e8c921fec4cb6ce
yonggyo1125 <yonggyo00@kakao.com>
2025-07-24 13:06:26 +0900

    .

---
+++ src/main/java/org/koreait/board/controllers/RequestBoard.java
@@ -0,0 +1,28 @@
+package org.koreait.board.controllers;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class RequestBoard {
+    private String mode;
+    private Long seq;
+
+    @NotBlank
+    private String bid;
+
+    @NotBlank
+    private String gid;
+
+    @NotBlank
+    private String poster;
+    private String guestPw;
+
+    @NotBlank
+    private String subject;
+
+    @NotBlank
+    private String content;
+    private boolean notice; // 공지글 여부
+    private boolean secret; // 비밀글 여부
+}
