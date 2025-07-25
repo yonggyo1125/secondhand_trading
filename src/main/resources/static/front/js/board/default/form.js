0eeaa35f00361ad2d11309110b4f8d1f35421d39
yonggyo1125 <yonggyo00@kakao.com>
2025-07-25 13:09:07 +0900

    .

--- src/main/resources/static/front/js/board/default/form.js
+++ src/main/resources/static/front/js/board/default/form.js
@@ -20,16 +20,37 @@ function fileUploadCallback(items) {
 
     const { insertEditorImage } = commonLib;
 
+    const editorTarget = document.querySelector(".editor-files");
+    const attachTarget = document.querySelector(".attach-files");
+
+    const editor_tpl = document.getElementById("editor-tpl").innerHTML;
+    const attach_tpl = document.getElementById("attach-tpl").innerHTML;
+
+    const domParser = new DOMParser();
+
     const editorImages = [];
     for (const {seq, fileUrl, location, fileName } of items) {
+        let targetEl, tpl;
         switch (location) {
             case "editor":
                 editorImages.push(fileUrl);
+                targetEl = editorTarget;
+                tpl = editor_tpl;
                 break;
             default:
-                //
+                targetEl = attachTarget;
+                tpl = attach_tpl;
         }
 
-        insertEditorImage(editorImages);
+        insertEditorImage(editorImages); // 에디터에 이미지 추가
+
+        // 템플릿 치환
+        tpl = tpl.replace(/\[seq\]/g, seq)
+                .replace(/\[fileName\]/g, fileName)
+                .replace(/\[fileUrl\]/g, fileUrl);
+        const dom = domParser.parseFromString(tpl, "text/html");
+        const fileItem = dom.querySelector(".file-items");
+
+        targetEl.append(fileItem);
     }
 }
