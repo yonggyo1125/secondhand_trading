385c05901f43121b9c6103fd47daa75279a31052
yonggyo1125 <yonggyo00@kakao.com>
2025-08-06 10:45:22 +0900

    .

--- src/test/java/org/koreait/tests/Ex01.java
+++ src/test/java/org/koreait/tests/Ex01.java
@@ -1,10 +1,10 @@
 package org.koreait.tests;
 
 import org.junit.jupiter.api.Test;
+import org.koreait.testutils.MockMember;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -18,7 +18,8 @@ public class Ex01 {
 
     @Test
     //@WithMockUser(authorities = "ADMIN")
-    @WithUserDetails(value="user01@test.org")
+    //@WithUserDetails(value="user01@test.org")
+    @MockMember
     void test1() throws Exception {
         mockMvc.perform(get("/mypage"))
                 .andDo(print());
